d65cb9c4368401544ead04610240b5fc20e77100
abiusx <git@abiusx.com>
2015-11-11 14:54:48 -0500

    default to mysql

--- start.php
+++ start.php
@@ -5,8 +5,8 @@ if (defined("iframework_started")) return;
 define ("iframework_started",true);
 function setup()
 {
-	// i::__init("db",array(new iframework\Database("root","123456aB","iframework","mysql")));
-	i::__init("db",array(new Database("root","123456aB","iframework.sqlite3","sqlite")));
+	i::__init("db",array(new Database("root","123456aB","kiwictf","mysql")));
+	// i::__init("db",array(new Database("root","123456aB","iframework.sqlite3","sqlite")));
 }
 if (!file_exists(__DIR__."/.htaccess"))
 	echo "<a href='?__install'>Install Now</a>";
